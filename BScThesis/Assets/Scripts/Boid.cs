6ae4acf067d555ce0cbc679f304a931f6e9cdf2b
xmjx <mrc.jarz@gmail.com>
2018-10-25 18:04:07 +0200

    Add basic class declarations

---
+++ BScThesis/Assets/Scripts/Boid.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SteeringBehaviorsNS
+{
+
+    public class Boid : MonoBehaviour
+    {
+
+        public Vector2 Velocity { get; set; }
+        public Vector2 Heading { get; set; }
+        public Vector2 Side { get; set; }
+
+        public double Mass { get; set; }
+        public double MaxSpeed { get; set; }
+        public double MaxForce { get; set; }
+        public double MaxTurnRate { get; set; }
+
+        public SteeringBehaviors SteeringBehaviors { get; set; }
+
+        // Use this for initialization
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Vector2 SteeringForce = SteeringBehaviors.Calculate();
+        }
+    }
+}
---
+++ BScThesis/Assets/Scripts/SteeringBehaviors.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SteeringBehaviorsNS
+{
+
+    public class SteeringBehaviors : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+        
+        private Vector2 Seek (Vector2 target)
+        {
+            return new Vector2();
+        }
+
+        private Vector2 Flee (Vector2 threat)
+        {
+            return new Vector2();
+        }
+
+        private Vector2 Arrive (Vector2 target)
+        {
+            return new Vector2();
+        }
+
+        public Vector2 Calculate()
+        {
+            return new Vector2();
+        }
+
+        public Vector2 ForwardComponent()
+        {
+            return new Vector2();
+        }
+
+        public Vector2 SideComponent()
+        {
+            return new Vector2();
+        }
+
+        public void SetPath()
+        {
+
+        }
+
+        public void SetTarget(Vector2 target)
+        {
+
+        }
+
+        public void SetTargetAgent1(Boid agent1)
+        {
+
+        }
+
+        public void SetTargetAgent2(Boid agent2)
+        {
+
+        }
+
+        public void SeekOn()
+        {
+
+        }
+
+        public void FleeOn()
+        {
+
+        }
+
+        public void ArriveOn()
+        {
+
+        }
+
+        public void SeekOff()
+        {
+
+        }
+
+        public void FleeOff()
+        {
+
+        }
+
+        public void ArriveOff()
+        {
+
+        }
+    }
+}
