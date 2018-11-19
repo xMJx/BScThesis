857aa2ff86380db6263b0ba4007004f87c3e4567
xmjx <mrc.jarz@gmail.com>
2018-11-19 15:29:35 +0100

    Add rotation towards direction

--- BScThesis/Assets/Scripts/Boid.cs
+++ BScThesis/Assets/Scripts/Boid.cs
@@ -8,27 +8,65 @@ namespace SteeringBehaviorsNS
     public class Boid : MonoBehaviour
     {
 
-        public Vector2 Velocity { get; set; }
-        public Vector2 Heading { get; set; }
-        public Vector2 Side { get; set; }
+        public Vector2 Velocity;
+        public Vector2 Heading;
+        public Vector2 Side;
 
-        public double Mass { get; set; }
-        public double MaxSpeed { get; set; }
-        public double MaxForce { get; set; }
-        public double MaxTurnRate { get; set; }
+        public GameObject TempTarget;
 
-        public SteeringBehaviors SteeringBehaviors { get; set; }
+        public float Mass { get; set; }
+        public float MaxSpeed { get; set; }
+        public float MaxForce { get; set; }
+        public float MaxTurnRate { get; set; }
+
+        public SteeringBehaviors SteeringBehaviors = new SteeringBehaviors();
 
         // Use this for initialization
         void Start()
         {
-            
+            MaxTurnRate = 1.0f;
+            Heading = new Vector2(0.0f, 1.0f);
         }
 
         // Update is called once per frame
         void Update()
         {
-            Vector2 SteeringForce = SteeringBehaviors.Calculate();
+            //Vector2 SteeringForce = SteeringBehaviors.Calculate();
+
+            RotateHeadingToFacePosition(TempTarget.transform.position);
+            RotateBoidToMatchHeading();
         }
+
+        bool RotateHeadingToFacePosition(Vector2 target)
+        {
+            // Normalizowany wektor od boidu do celu
+            Vector2 toTarget = (target - (Vector2)transform.position).normalized;
+
+            float angle = Vector2.SignedAngle(Heading, toTarget);
+
+            if (angle > 0)
+                angle = Mathf.Min(angle, MaxTurnRate);
+            else
+                angle = -Mathf.Min(-angle, MaxTurnRate);
+
+            //Heading = toTarget;
+            Heading.Normalize();
+            
+            Heading = Quaternion.Euler(0, 0, angle) * Heading;
+
+
+            return false;
+        }
+
+        bool RotateBoidToMatchHeading()
+        {
+            if (transform.rotation.)
+                return true;
+
+            transform.rotation = Quaternion.EulerRotation(0,0,-Mathf.Atan2(Heading.x, Heading.y));
+
+            return false;
+        }
+
     }
 }
