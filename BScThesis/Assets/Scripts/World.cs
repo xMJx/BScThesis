a9cba336a522aa637fb2a4b3eb58621c32d501c1
MJ-PC\Marcin <mrc.jarz@gmail.com>
2019-01-08 10:16:59 +0100

    Add first GPU assets

--- BScThesis/Assets/Scripts/World.cs
+++ BScThesis/Assets/Scripts/World.cs
@@ -9,29 +9,44 @@ namespace SteeringBehaviorsNS
 
     public class World : MonoBehaviour
     {
+        struct BoidData
+        {
+            public Vector2 pos;
+            public Vector2 vel;
+        }
+
         private List<Boid> boids;
-        private float[] boidsData;
-        private ComputeBuffer computeBuffer;
+        private ComputeBuffer boidDataBuffer;
+
+        public ComputeShader SteeringBehaviorsShader;
         
         // Use this for initialization
         void Start()
         {
             boids = FindObjectsOfType<Boid>().ToList();
-            computeBuffer = new ComputeBuffer(boids.Count, 16);
-            boidsData = new float[boids.Count * 4];
+            boidDataBuffer = new ComputeBuffer(boids.Count, sizeof(float) * 4);
         }
 
         // Update is called once per frame
         void Update()
         {
+            BoidData[] boidsData = new BoidData[boids.Count];
+
             for (int i=0; i<boids.Count; i++)
             {
-                boidsData[4 * i] = boids[i].transform.position.x;
-                boidsData[4 * i + 1] = boids[i].transform.position.y;
-                boidsData[4 * i + 2] = boids[i].Velocity.x;
-                boidsData[4 * i + 3] = boids[i].Velocity.y;
+                BoidData bd = new BoidData();
+                bd.pos = boids[i].transform.position;
+                bd.vel = boids[i].Velocity;
+
+                boidsData[i] = bd;
             }
-            computeBuffer.SetData(boidsData);
+            boidDataBuffer.SetData(boidsData);
+
+            // compute shader here
+            int kernelIndex = SteeringBehaviorsShader.FindKernel("CSMain");
+            SteeringBehaviorsShader.SetBuffer(kernelIndex, "BoidDataBuffer", boidDataBuffer);
+
+
         }
     }
 }
